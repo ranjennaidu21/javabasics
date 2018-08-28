31b42f5cd21f27bc950a091aacd639bfaedb2f18
ranje <ranje@LAPTOP-4IAQGGJI>
2018-08-28 12:08:18 +0800

    FileReader to read text file and about Closable interface

---
+++ JavaBasics/src/forty/inputoutput/filereader/closeable/Locations.java
@@ -0,0 +1,157 @@
+package forty.inputoutput.filereader.closeable;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+
+
+public class Locations implements Map<Integer, Location> {
+    private static Map<Integer, Location> locations = new HashMap<Integer, Location>();
+
+    public static void main(String[] args) throws IOException {
+        try(FileWriter locFile = new FileWriter("locations.txt");
+            FileWriter dirFile = new FileWriter("directions.txt")) {
+            for(Location location : locations.values()) {
+                locFile.write(location.getLocationID() + "," + location.getDescription() + "\n");
+                for(String direction : location.getExits().keySet()) {
+                    dirFile.write(location.getLocationID() + "," + direction + "," + location.getExits().get(direction) + "\n");
+                }
+            }
+        }
+//        FileWriter locFile = null;
+//        try {
+//            locFile = new FileWriter("locations.txt");
+//            for(Location location : locations.values()) {
+//                locFile.write(location.getLocationID() + "," + location.getDescription() + "\n");
+//            }
+//        } finally {
+//            System.out.println("in finally block");
+//            if (locFile != null) {
+//                System.out.println("Attempting to close locfile");
+//                locFile.close();
+//            }
+//        }
+    }
+
+    static {
+    	
+    	//Now use FileReader to read the file from what created in previou FileWriter package
+    	//instead of adding manually to the static locations mapping as below
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(new FileReader("locations.txt"));
+            //get each string which is separated by coma
+            scanner.useDelimiter(",");
+            while(scanner.hasNextLine()) {
+                int loc = scanner.nextInt();
+                //skip over the coma to go to the next section
+                scanner.skip(scanner.delimiter());
+                String description = scanner.nextLine();
+                System.out.println("Imported loc: " + loc + ": " + description);
+                Map<String, Integer> tempExit = new HashMap<>();
+                locations.put(loc, new Location(loc, description, tempExit));
+            }
+
+        } catch(IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(scanner != null) {
+            	//why filereader object is not closed? because..
+            	//when the scanner method is closed, it also close any other stream together
+            	//provided the stream object implements the closable interface
+                scanner.close();
+            }
+        }
+
+//        Map<String, Integer> tempExit = new HashMap<String, Integer>();
+//        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java",null));
+//
+//        tempExit = new HashMap<String, Integer>();
+//        tempExit.put("W", 2);
+//        tempExit.put("E", 3);
+//        tempExit.put("S", 4);
+//        tempExit.put("N", 5);
+//        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building",tempExit));
+//
+//        tempExit = new HashMap<String, Integer>();
+//        tempExit.put("N", 5);
+//        locations.put(2, new Location(2, "You are at the top of a hill",tempExit));
+//
+//        tempExit = new HashMap<String, Integer>();
+//        tempExit.put("W", 1);
+//        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring",tempExit));
+//
+//        tempExit = new HashMap<String, Integer>();
+//        tempExit.put("N", 1);
+//        tempExit.put("W", 2);
+//        locations.put(4, new Location(4, "You are in a valley beside a stream",tempExit));
+//
+//        tempExit = new HashMap<String, Integer>();
+//        tempExit.put("S", 1);
+//        tempExit.put("W", 2);
+//        locations.put(5, new Location(5, "You are in the forest",tempExit));
+
+    }
+    @Override
+    public int size() {
+        return locations.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return locations.isEmpty();
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return locations.containsKey(key);
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return locations.containsValue(value);
+    }
+
+    @Override
+    public Location get(Object key) {
+        return locations.get(key);
+    }
+
+    @Override
+    public Location put(Integer key, Location value) {
+        return locations.put(key, value);
+    }
+
+    @Override
+    public Location remove(Object key) {
+        return locations.remove(key);
+    }
+
+    @Override
+    public void putAll(Map<? extends Integer, ? extends Location> m) {
+
+    }
+
+    @Override
+    public void clear() {
+        locations.clear();
+
+    }
+
+    @Override
+    public Set<Integer> keySet() {
+        return locations.keySet();
+    }
+
+    @Override
+    public Collection<Location> values() {
+        return locations.values();
+    }
+
+    @Override
+    public Set<Entry<Integer, Location>> entrySet() {
+        return locations.entrySet();
+    }
+}
